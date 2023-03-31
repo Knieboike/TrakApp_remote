1f8f52e36fc4b242b7020a3696c836d6053d1311
Nils <boike.nils@gmail.com>
2023-03-31 15:05:45 +0200

    Initial Commit

---
+++ TrakApp/TrakAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrakAppApp.swift
+//  TrakApp
+//
+//  Created by Nils Boike on 31.03.23.
+//
+
+import SwiftUI
+
+@main
+struct TrakAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
